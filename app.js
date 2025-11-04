ba98996e124665471ebedb42a8af3116d62fcd68
Vishal Gupta <your-email@example.com>
2025-11-04 20:37:28 +0530

    i add a file where i store all the frontend page first and gitignore it

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(session({
-  secret: "mySecretKey",      // any random string
+  secret: "mySecretKey",
   resave: false,
   saveUninitialized: true
 }));
