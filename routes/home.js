35772f5c0ff488747a6d7628149edbe89065fc1a
Vishal Gupta <your-email@example.com>
2025-11-08 12:10:42 +0530

    add middleware in single_post

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const router = express.Router();
 const Blog = require("../models/postData")
+const logger = require("../middleware/loggerMiddleware")
 
 router.get("/",async(req,res)=>{
     const blogs = await Blog.find();
     res.render("index.ejs",{blogs})
+})
+router.get("/post/:id",logger,async(req,res)=>{
+    const blog = await Blog.findById(req.params.id);
+    res.render("single_post.ejs",{blog})
 })
 
 module.exports = router;
