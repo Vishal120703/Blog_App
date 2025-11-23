367684541a2ce74abddc30c431b5e7b3a0c777fa
Vishal Gupta <your-email@example.com>
2025-11-23 23:10:22 +0530

    like without load

--- routes/home.js
+++ routes/home.js
@@ -14,28 +14,55 @@ router.get("/post/:id",logger,async(req,res)=>{
     res.render("single_post.ejs",{blog})
 })
 
+// router.post("/post/:id/like", logger, async (req, res) => {
+//   try {
+//     const blog = await Blog.findById(req.params.id);
+//     const user = req.user.username;
+
+//     // Initialize likedBy if it doesn't exist
+//     const likedBy = blog.likedBy || [];
+
+//     if (likedBy.includes(user)) {
+//       blog.likedBy = likedBy.filter(item => item !== user);
+//       console.log("User unliked this post");
+//     } else {
+//       blog.likedBy.push(user);
+//       console.log("User liked this post");
+//     }
+//     blog.likes = blog.likedBy.length
+
+//     await blog.save();
+//     res.redirect(`/post/${req.params.id}`);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Server error");
+//   }
+// });
+
 router.post("/post/:id/like", logger, async (req, res) => {
   try {
     const blog = await Blog.findById(req.params.id);
     const user = req.user.username;
 
-    // Initialize likedBy if it doesn't exist
-    const likedBy = blog.likedBy || [];
-
-    if (likedBy.includes(user)) {
-      blog.likedBy = likedBy.filter(item => item !== user);
-      console.log("User unliked this post");
-    } else {
+    if (!blog.likedBy.includes(user)) {
       blog.likedBy.push(user);
-      console.log("User liked this post");
+    } else {
+      blog.likedBy = blog.likedBy.filter(u => u !== user);
     }
-    blog.likes = blog.likedBy.length
 
+    blog.likes = blog.likedBy.length;
     await blog.save();
-    res.redirect(`/post/${req.params.id}`);
+
+    // IMPORTANT: return JSON instead of redirect
+    res.json({
+      success: true,
+      likes: blog.likes,
+      liked: blog.likedBy.includes(user)
+    });
+
   } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
+    console.log(err);
+    res.status(500).json({ success: false });
   }
 });
 router.post("/post/:id/comment",logger,async(req,res)=>{
